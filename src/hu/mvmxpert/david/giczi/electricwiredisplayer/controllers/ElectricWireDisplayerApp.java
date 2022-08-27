9dd66963ce44f3b6f8839067801aea8f4de51ff6
david.giczi@gmail.com <david.giczi@gmail.com>
2022-08-27 13:09:50 +0200

    first line

--- src/hu/mvmxpert/david/giczi/electricwiredisplayer/controllers/ElectricWireDisplayerApp.java
+++ src/hu/mvmxpert/david/giczi/electricwiredisplayer/controllers/ElectricWireDisplayerApp.java
@@ -5,14 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class ElectricWireDisplayerApp extends Application {
 
+	private void drawLine(StackPane root) {
+		Line line = new Line(100, 100, 500, 500);
+		line.getStrokeDashArray().addAll(25d, 10d);
+		line.setStrokeWidth(5);
+		line.setStroke(Color.RED);
+		root.getChildren().add(line);
+	}
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			StackPane root = FXMLLoader.load(getClass().getResource("displayer.fxml"));
+			drawLine(root);
 			Scene scene = new Scene(root);
 			primaryStage.setMaximized(true);
 			primaryStage.setResizable(false);
